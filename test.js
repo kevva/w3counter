e7808f4c78575de96e336f0bc0aa3a0c8f0be6d7
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-13 02:19:46 +0200

    Show errors in tests

--- test.js
+++ test.js
@@ -7,7 +7,7 @@ test('fetch 10 web browsers', function (t) {
 	t.plan(2);
 
 	w3counter('browser', function (err, data) {
-		t.assert(!err);
+		t.assert(!err, err);
 		t.assert(data.length === 10);
 	});
 });
@@ -16,7 +16,7 @@ test('fetch 10 countries', function (t) {
 	t.plan(2);
 
 	w3counter('country', function (err, data) {
-		t.assert(!err);
+		t.assert(!err, err);
 		t.assert(data.length === 10);
 	});
 });
@@ -25,7 +25,7 @@ test('fetch 10 operating systems', function (t) {
 	t.plan(2);
 
 	w3counter('os', function (err, data) {
-		t.assert(!err);
+		t.assert(!err, err);
 		t.assert(data.length === 10);
 	});
 });
@@ -34,7 +34,7 @@ test('fetch 10 screen resolutions', function (t) {
 	t.plan(2);
 
 	w3counter('res', function (err, data) {
-		t.assert(!err);
+		t.assert(!err, err);
 		t.assert(data.length === 10);
 	});
 });
