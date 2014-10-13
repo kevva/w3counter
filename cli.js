831891564e63bcb0c0bf7bcf3ded83d4f0ff8b08
Kevin Martensson <kevinmartensson@gmail.com>
2014-10-14 00:10:34 +0200

    Use `meow` in CLI

--- cli.js
+++ cli.js
@@ -1,19 +1,15 @@
 #!/usr/bin/env node
 'use strict';
 
-var input = process.argv.slice(2);
-var pkg = require('./package.json');
+var meow = require('meow');
 var w3counter = require('./');
 
 /**
- * Help screen
+ * Initialize CLI
  */
 
-function help() {
-	console.log([
-		'',
-		pkg.description,
-		'',
+var cli = meow({
+	help: [
 		'  Usage',
 		'    w3counter <type>',
 		'',
@@ -22,47 +18,14 @@ function help() {
 		'    w3counter country',
 		'    w3counter os',
 		'    w3counter res'
-	].join('\n'));
-}
-
-/**
- * Show help
- */
-
-if (input.indexOf('-h') !== -1 || input.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-/**
- * Show package version
- */
-
-if (input.indexOf('-v') !== -1 || input.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}
-
-/**
- * Show error if no type is provided
- */
-
-if (!input.length) {
-	console.error([
-		'Please provide a type. Available types are:',
-		'  browser — Ten most popular web browsers',
-		'  country — Ten most popular countries',
-		'  os — Ten most popular operating systems',
-		'  res — Ten most popular screen resolutions'
-	].join('\n'));
-	process.exit(1);
-}
+	].join('\n')
+});
 
 /**
  * Run
  */
 
-w3counter(input, function (err, types) {
+w3counter(cli.input, function (err, types) {
 	if (err) {
 		console.error(err.message);
 		process.exit(1);
